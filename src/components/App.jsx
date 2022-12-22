fe6f3cea9827b22ae2af3c836743b16b64bde995
Tomasz Skolimo <skolim10@gmail.com>
2022-12-22 13:21:12 +0100

    refaktor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,43 @@
 import React, { useState, useEffect } from 'react';
-import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
+import { useSelector, useDispatch } from 'react-redux';
+import { getContacts, getFilter } from 'redux/selectors';
+import { addContact, delContact } from 'redux/actions';
+
+// const getFromLocalStorage = key => {
+//   const contactsFromLocalStorage = localStorage.getItem(key);
+
+//   return contactsFromLocalStorage
+//     ? JSON.parse(contactsFromLocalStorage)
+//     : hardCodedContact;
+// };
+
+// const hardCodedContact = [
+//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+// ];
 
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
 
-  const [firstRenderFlag, setFlag] = useState(true);
-
-  const [filter, setFilter] = useState('');
-  useEffect(() => {}, []);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    if (firstRenderFlag) {
-      const contactsFromLocalStorage = localStorage.getItem('contactList');
+    localStorage.setItem('contactList', JSON.stringify(contacts));
+  }, [contacts]);
 
-      if (contactsFromLocalStorage !== 'undefined') {
-        const parsedContacts = JSON.parse(contactsFromLocalStorage);
-
-        if (parsedContacts) {
-          setContacts(parsedContacts);
-        }
-      }
-      setFlag(false);
-    } else {
-      localStorage.setItem('contactList', JSON.stringify(contacts));
-    }
-  }, [contacts, firstRenderFlag]);
-
-  const handleChange = e => {
-    const { value } = e.target;
-    setFilter(value);
-  };
+  // const handleChange = e => {
+  //   const { value } = e.target;
+  //   setFilter(value);
+  // };
 
   const handleSubmit = e => {
-    const id = nanoid();
+    // const id = nanoid();
     const name = e.name;
     const number = e.number;
     const contactsLists = [...contacts];
@@ -48,20 +45,18 @@ export const App = () => {
     if (contactsLists.findIndex(contact => name === contact.name) !== -1) {
       alert(`${name} is already in contacts.`);
     } else {
-      contactsLists.push({ id, name, number });
+      dispatch(addContact(name, number));
     }
-
-    setContacts(contactsLists);
   };
 
   const handleDelete = e => {
-    setContacts(contacts.filter(contact => contact.id !== e));
+    dispatch(delContact(e));
   };
 
   const getFilteredContacts = () => {
-    const filterContactsList = contacts.filter(contact => {
-      return contact.name.toLowerCase().includes(filter.toLowerCase());
-    });
+    const filterContactsList = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
 
     return filterContactsList;
   };
@@ -81,7 +76,7 @@ export const App = () => {
       <h1>Phonebook</h1>
       <ContactForm handleSubmit={handleSubmit} />
       <h2> Contacts</h2>
-      <Filter filter={filter} handleChange={handleChange} />
+      <Filter />
       <ContactList
         contacts={getFilteredContacts()}
         handleDelete={handleDelete}
